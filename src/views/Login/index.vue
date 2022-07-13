0cf7dedbbfa8d097d2130c22a73cdc6cda51fcac
doublekou <951513186@qq.com>
2022-07-13 18:17:52 +0800

    login页面完成一半

---
+++ src/views/Login/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <van-nav-bar
+      class="navbar"
+      title="标题"
+      left-arrow
+      @click-left="backToPrePage"
+    >
+      <template #left>
+        <van-icon name="cross" />
+      </template>
+    </van-nav-bar>
+    <van-form @submit="login" class="form">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        type="text"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+        <template #right-icon>
+          <van-button class="code-btn" size="mini" round>发送验证码</van-button>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+import { Toast } from 'vant'
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onClickLeft() {
+      Toast('返回')
+    },
+    backToPrePage() {
+      // console.log(this);
+      this.$router.back()
+    },
+    async login() {
+      const res = await login(this.mobile, this.code)
+      console.log(res)
+      // console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.navbar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+  .van-icon {
+    color: #fff;
+  }
+}
+.form {
+  :deep(.van-field__label) {
+    flex: 1;
+  }
+  :deep(.van-field__value) {
+    flex: 20;
+  }
+  .toutiao {
+    font-size: 0.49333rem;
+  }
+  .code-btn {
+    background-color: #eee;
+    padding: 0 10px;
+    color: #666;
+  }
+}
+</style>
