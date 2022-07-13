ca03d75a78b4ef0fb121bc8786cabd2a56a003eb
doublekou <951513186@qq.com>
2022-07-13 12:10:03 +0800

    完成项目初始化

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': {
+      browsers: ['Android >= 4.0', 'iOS >= 8']
+    },
+    'postcss-pxtorem': {
+      rootValue: (module) => {
+        if (/vant/gi.test(module.file)) {
+          return 37.5
+        } else {
+          return 75
+        }
+      },
+      propList: ['*']
+    }
+  }
+}
+
