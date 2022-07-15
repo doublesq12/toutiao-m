e70ffb9bde111e5a1090c2597f8bf198995b7571
doublekou <951513186@qq.com>
2022-07-15 16:21:58 +0800

    login页面完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -10,12 +10,12 @@
         <van-icon name="cross" />
       </template>
     </van-nav-bar>
-    <van-form @submit="login" class="form">
+    <van-form @submit="login" class="form" ref="form">
       <van-field
         v-model="mobile"
         name="mobile"
         placeholder="请输入手机号"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        :rules="mobileRules"
       >
         <template #label>
           <span class="toutiao toutiao-shouji"></span>
@@ -26,43 +26,110 @@
         type="text"
         name="code"
         placeholder="请输入验证码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        :rules="codeRules"
       >
         <template #label>
           <span class="toutiao toutiao-yanzhengma"></span>
         </template>
         <template #right-icon>
-          <van-button class="code-btn" size="mini" round>发送验证码</van-button>
+          <van-count-down
+            v-if="isShowCountDown"
+            :time="3 * 1000"
+            @finish="isShowCountDown = false"
+          />
+          <van-button
+            v-else
+            @click="sendCode"
+            class="code-btn"
+            size="mini"
+            round
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
       <div style="margin: 16px">
         <van-button block type="info" native-type="submit">登录</van-button>
       </div>
     </van-form>
+    <h1>{{ $store.state.number }}</h1>
+    <button @click="increment">自增</button>
   </div>
 </template>
 
 <script>
-import { login } from '@/api/user'
+import { login, sendCode } from '@/api/user'
 import { Toast } from 'vant'
+import { mobileRules, codeRules } from './rules'
 export default {
   data() {
     return {
       mobile: '',
-      code: ''
+      code: '',
+      mobileRules,
+      codeRules,
+      isShowCountDown: false
     }
   },
   methods: {
     onClickLeft() {
       Toast('返回')
     },
+    increment() {
+      // this.$store.commit('setNumber')
+    },
     backToPrePage() {
       // console.log(this);
       this.$router.back()
     },
+    async sendCode() {
+      try {
+        await this.$refs.form.validate('mobile')
+        await sendCode(this.mobile)
+        this.isShowCountDown = true
+      } catch (error) {
+        // this.$toast.fail('手机号非法！')
+        if (!error.response) {
+          this.$toast.fail('手机号格式不正确')
+        } else {
+          const status = error.response.status
+          if (status === 404 || status === 429) {
+            this.$toast.fail(error.response.data.message)
+          }
+        }
+      }
+    },
     async login() {
-      const res = await login(this.mobile, this.code)
-      console.log(res)
+      this.$toast.loading({
+        message: '不要着急.加载中..',
+        forbidClick: true
+      })
+      try {
+        const res = await login(this.mobile, this.code)
+        // console.log(res)
+        this.$store.commit('setUser', res.data.data)
+        this.$router.push('/profile')
+        this.$toast.success('登录成功')
+      } catch (error) {
+        // this.$toast.fail('登录失败')
+        // console.log(error)
+        const status = error.response.status
+        let message = '登录错误，请刷新～'
+        if (status === 400) {
+          message = error.response.message
+        }
+        this.$toast.fail(message)
+        // switch (status) {
+        //   case 400:
+        //     this.$toast.fail(error.response.data.message)
+        //     break
+        //   case 507:
+        //     this.$toast.fail('登录错误，请刷新～')
+        //     break
+        //   default:
+        //     this.$toast.fail('登录错误，请刷新～')
+        //     break
+        // }
+      }
       // console.log('submit', values)
     }
   }
---
+++ src/views/Login/rules.js
@@ -0,0 +1,12 @@
+export const mobileRules = [
+  { required: true, message: '请填写用户名' },
+  {
+    pattern:
+      /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+    message: '手机号格式不正确！'
+  }
+]
+export const codeRules = [
+  { required: true, message: '请填写密码' },
+  { pattern: /^[0-9]{6}$/, message: '请输入6位验证码' }
+]
